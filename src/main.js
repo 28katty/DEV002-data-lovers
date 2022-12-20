f8917848b1bce2e37d67d38921034c476b6aa699
katterin olivares <katterinolivaresalcalde28@gmail.com>
2022-12-20 12:00:49 -0500

    recorte de codigo y modificacion

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 
 import { peticion } from "./script/data.js";
-import { champDat } from './data/lol/lol.js'
 import { showMenu } from "./script/menu.js";
-import { filter } from "./script/filtros.js";
 import { search } from "./script/busqueda.js";
-import { ordenarAz } from "./script/ordenar-az.js";
-import { ordenarZa } from "./script/ordenar-za.js";
-
-
-const championArray = Object.values(champDat[0].data)
-
-
-peticion(championArray)
+peticion()
 showMenu()
-filter()
 search()
-ordenarAz(championArray)
-ordenarZa(championArray)
--- src/script/data.js
+++ src/script/data.js
@@ -1,28 +1,121 @@
+import { champDat } from '.././data/lol/lol.js'
 
-export function peticion(data){
+const championArray = Object.values(champDat[0].data)
 
-  const championUser = () => {
-      const contenedor = document.getElementById("gallery")
-    const fragment = document.createDocumentFragment()
-    const templateGallery = document.getElementById('template-gallery').content;
 
-    const championRandom = data.slice().sort(() => Math.random() - 0.5);
+export function peticion(){
 
-    for (const item of championRandom) {
-      templateGallery.querySelector('.gallery__img').src = item.splash
-      templateGallery.querySelector('.gallery__name').textContent=item.name
-      templateGallery.querySelector('.gallery__surname').textContent=item.title.toUpperCase()
-      templateGallery.querySelector('.gallery__attack').textContent=('Attack: ' + (item.info.attack))
-      templateGallery.querySelector('.gallery__defense').textContent=('Defense: ' + (item.info.defense))
-      templateGallery.querySelector('.gallery__magic').textContent=('Magic: ' + (item.info.magic))
-      templateGallery.querySelector('.gallery__difficulty').textContent=('Difficulty: ' + (item.info.difficulty))
-      templateGallery.querySelector('.gallery__tags').textContent=item.tags
+  const championUser = (data) => {
 
-      const clone = templateGallery.cloneNode(true)
-      fragment.append(clone)
+    const contenedor = document.getElementById("gallery")
+    // const championRandom = data.slice().sort(() => Math.random() - 0.5);
+
+    let cardHTML = "";
+      for (const item of data) {
+      cardHTML += `
+        <div class="gallery__card">
+          <div class="gallery__info">
+            <img src="${item.splash}" alt="" class="gallery__img" />
+            <h2 class="gallery__name">${item.name}</h2>
+            <h4 class="gallery__tags">${item.tags}</h4>
+          </div>
+          <div class="gallery__reverse">
+            <h3 class="gallery__surname">${item.title.toUpperCase()}</h3><br>
+            <h4 class="gallery__attack">Attack: ${item.info.attack}</h4>
+            <h4 class="gallery__defense">Defense: ${item.info.defense}</h4>
+            <h4 class="gallery__magic">Magic: ${item.info.magic}</h4>
+            <h4 class="gallery__difficulty">Difficulty: ${
+              item.info.difficulty
+            }</h4>
+          </div>
+        </div>
+    `;
     }
-    contenedor.append(fragment)
+    contenedor.innerHTML = cardHTML;
+
     }
-    championUser()
+    championUser(championArray)
+
+    // eventos
+    const fighterBtn = document.getElementById('fighter');
+    const tankBtn = document.getElementById('tank');
+    const mageBtn = document.getElementById('mage');
+    const assasinBtn = document.getElementById('assasin');
+    const supportBtn = document.getElementById('support');
+    const marksmanBtn = document.getElementById('marksman');
+    const azBtn = document.getElementById("az")
+     const zaBtn = document.getElementById("za")
+
+
+
+  // remover elementos
+  const removeElement = () => {
+    let cards = document.querySelectorAll(".gallery__card");
+    cards.forEach((item) => item.remove());
+  };
+// funcion  filter
+  const filter = (data, textFilter) => {
+    const filtertext = data.filter((item) => item.tags.includes(textFilter));
+    championUser(filtertext);
+  };
+  // ordenar
+  const ordenar = (data) => {
+    const ordenarAz = data.slice().sort((a,b) => {
+      if (a[0] < b[0]) {
+    return -1;
+  }
+  if (a[0] > b[0]) {
+    return 1;
+  }
+  return 0;
+    })
+    championUser(ordenarAz)
+  }
+
+// Eventos de filtrado
+  fighterBtn.addEventListener("click", () => {
+      removeElement()
+      filter(championArray,'Fighter')
+  });
+
+  tankBtn.addEventListener("click", ()=>{
+    removeElement()
+    filter(championArray,'Tank')
+  })
+
+  mageBtn.addEventListener("click", ()=>{
+    removeElement()
+    filter(championArray,'Mage')
+  })
+
+  assasinBtn.addEventListener("click", ()=>{
+    removeElement()
+    filter(championArray,'Assassin')
+  })
+
+  supportBtn.addEventListener("click",()=>{
+    removeElement()
+    filter(championArray,'Support')
+  })
+
+  marksmanBtn.addEventListener("click",()=>{
+    removeElement()
+    filter(championArray,'Marksman')
+  })
+
+
+azBtn.addEventListener('click',() => {
+    removeElement()
+    // championUser(championArray)
+    championUser(championArray.slice().sort())
+  })
+zaBtn.addEventListener('click',() => {
+  removeElement()
+    championUser(championArray.slice().reverse())
+  })
+
+
+
 }
+
 
